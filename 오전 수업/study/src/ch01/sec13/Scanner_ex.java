c7201ba52db75ec32f38a2bdf185734ce31068bf
Hyejin Kim <chol159156@gmail.com>
2025-04-15 13:09:26 +0900

    2025.04.15 오전수업

---
+++ 오전 수업/study/src/ch01/sec13/Scanner_ex.java
@@ -0,0 +1,9 @@
+package ch01.sec13;
+
+import java.util.Scanner;
+
+public class Scanner_ex {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
---
+++ 오전 수업/study/src/ch05/argTest.java
@@ -0,0 +1,8 @@
+package ch05;
+
+public class argTest {
+    public static void main(String[] args) {
+        System.out.println(args.length);
+        System.out.println(args[0]);
+    }
+}
