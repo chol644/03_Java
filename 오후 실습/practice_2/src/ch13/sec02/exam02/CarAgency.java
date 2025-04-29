020d1c3598c774d55d297d83269ddebb73d2c33a
Hyejin Kim <chol159156@gmail.com>
2025-04-29 16:19:05 +0900

    2025.04.29 심화

--- 오후 실습/practice_2/src/ch13/sec02/exam02/CarAgency.java
+++ 오후 실습/practice_2/src/ch13/sec02/exam02/CarAgency.java
@@ -1,6 +1,6 @@
 package ch13.sec02.exam02;
 
-public class CarAgency implements Rentable<Car> { // 타입파라미터 P를 Car으로 대체
+public class CarAgency extends Rentable<Car> { // 타입파라미터 P를 Car으로 대체
     @Override
     public Car rent() {
         return new Car();
