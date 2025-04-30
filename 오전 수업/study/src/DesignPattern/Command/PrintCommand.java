5030e87347f4f0e9b0cbc01c98e419c07ea25f7c
Hyejin Kim <chol159156@gmail.com>
2025-04-30 11:49:51 +0900

    2025.04.30 command

---
+++ 오전 수업/study/src/DesignPattern/Command/PrintCommand.java
@@ -0,0 +1,11 @@
+package DesignPattern.Command;
+
+public class PrintCommand implements Command {
+    @Override
+    public void execute() {
+        System.out.println("===============");
+        System.out.println("Print command");
+        System.out.println("===============");
+        System.out.println();
+    }
+}
