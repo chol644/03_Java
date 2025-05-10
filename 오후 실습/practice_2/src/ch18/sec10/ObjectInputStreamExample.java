29e60797d242dc0ee073ecaf84bc755fde6d8dc9
Hyejin Kim <chol159156@gmail.com>
2025-05-10 15:14:01 +0900

    2025.05.10

---
+++ 오후 실습/practice_2/src/ch18/sec10/ObjectInputStreamExample.java
@@ -0,0 +1,13 @@
+package ch18.sec10;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Arrays;
+
+public class ObjectInputStreamExample {
+    public static void main(String[] args) throws Exception {
+
+    }
+}
